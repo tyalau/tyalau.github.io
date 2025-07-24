29010bea917a8fd87c26d2e9c79726f502ff088f
Amy Lau <tszyanalau@gmail.com>
2025-07-24 22:41:09 +0900

    chore: add eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,46 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript')]
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript'],
+    rules: {
+      'array-callback-return': 'error',
+      'dot-notation': 'error',
+      'func-style': ['error', 'expression'],
+      'no-else-return': [
+        'error',
+        {
+          allowElseIf: false,
+        },
+      ],
+      'no-lonely-if': 'error',
+      'no-multi-assign': 'error',
+      'no-negated-condition': 'error',
+      'no-param-reassign': 'error',
+      'no-return-assign': 'error',
+      'no-undef-init': 'error',
+      'no-unneeded-ternary': 'error',
+      'no-unused-expressions': 'error',
+      'object-shorthand': 'error',
+      'prefer-arrow-callback': 'error',
+      'prefer-destructuring': [
+        'error',
+        {
+          object: true,
+        },
+      ],
+      'prefer-regex-literals': 'error',
+      'prefer-template': 'error',
+
+      'react/button-has-type': 'error',
+      'react/jsx-boolean-value': 'error',
+      'react/jsx-no-useless-fragment': 'error',
+      'react/no-array-index-key': 'error',
+      'react/self-closing-comp': 'error',
+      'react/state-in-constructor': 'error',
+    },
+  }),
+]
 
 export default eslintConfig
