c7a369dc834f6b5c211cd56f3f52e16d15b46158
Amy Lau <tszyanalau@gmail.com>
2025-07-25 22:26:00 +0900

    chore: add env vars

---
+++ env.d.ts
@@ -0,0 +1,9 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_GITHUB_TOKEN: string
+    NEXT_PUBLIC_GITHUB_USERNAME: string
+    NEXT_PUBLIC_LINKEDIN_USERNAME: string
+    NEXT_PUBLIC_MEDIUM_USERNAME: string
+    NEXT_PUBLIC_PROFILE_DATA_URL: string
+  }
+}
