5d6ecafde882fc751107a6eb91d1b0c226246821
Amy Lau <tszyanalau@gmail.com>
2025-09-12 20:44:58 +0900

    fix: prevent cache in  API fetch calls with date parameter

--- src/containers/Blog/index.tsx
+++ src/containers/Blog/index.tsx
@@ -2,11 +2,15 @@ import Posts from '@/containers/Blog/Posts'
 import { Blog } from '@/types'
 
 export default async function Blogs() {
-  const res = await fetch(
-    `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/${process.env.NEXT_PUBLIC_MEDIUM_USERNAME}`
-  )
+  const url = new URL('https://api.rss2json.com/v1/api.json')
+  url.searchParams.set('rss_url', `https://medium.com/feed/${process.env.NEXT_PUBLIC_MEDIUM_USERNAME}`)
+  url.searchParams.set('v', Date.now().toString())
+
+  const res = await fetch(url.toString())
 
   const data = await res.json()
+
+  if (!res.ok) return null
 
   const blogs: Blog[] = data.items
 
